99a153cdc1bab86749edf3be393ac06ec30c7f4d
mash <mlamisa@students.cs.ubc.ca>
2020-02-27 00:42:47 +0800

    moved testfile and changed saving location to data dir

--- src/test/model/FileOperatorTest.java
+++ src/test/model/FileOperatorTest.java
@@ -31,7 +31,7 @@ public class FileOperatorTest {
     @Test
     public void testLoad() {
         try {
-            System.setIn(new ByteArrayInputStream("loadtest.csv\nY\n".getBytes()));
+            System.setIn(new ByteArrayInputStream("./data/loadtest.csv\nY\n".getBytes()));
             app.loadAccounts();
             assertEquals(2, app.getAccounts().size());
             assertEquals("Testing Purpose", app.getAccounts().get(0).description);
@@ -44,7 +44,7 @@ public class FileOperatorTest {
     @Test
     public void testLoadFail() {
         assertThrows(FileNotFoundException.class, () -> {
-            System.setIn(new ByteArrayInputStream("nonexist.csv\nY\n".getBytes()));
+            System.setIn(new ByteArrayInputStream("./data/nonexist.csv\nY\n".getBytes()));
             app.loadAccounts();
         });
     }
@@ -52,9 +52,9 @@ public class FileOperatorTest {
     @Test
     public void testSave() {
         try {
-            System.setIn(new ByteArrayInputStream("savetest.csv".getBytes()));
+            System.setIn(new ByteArrayInputStream("./data/savetest.csv".getBytes()));
             app.saveAccounts();
-            File savedFile = new File("savetest.csv");
+            File savedFile = new File("./data/savetest.csv");
             Scanner scanner = new Scanner(savedFile);
             String expectedFirstLine = "Testing Purpose,2000.00,2";
             assertEquals(scanner.nextLine(), expectedFirstLine);
@@ -72,9 +72,9 @@ public class FileOperatorTest {
             app.addTransaction();
             System.setIn(new ByteArrayInputStream("2\n1\n150\nGov Incentive\n".getBytes()));
             app.addTransaction();
-            System.setIn(new ByteArrayInputStream("savetest2.csv".getBytes()));
+            System.setIn(new ByteArrayInputStream("./data/savetest2.csv".getBytes()));
             app.saveAccounts();
-            File savedFile = new File("savetest2.csv");
+            File savedFile = new File("./data/savetest2.csv");
             Scanner scanner = new Scanner(savedFile);
             String expectedFirstLine = "Testing Purpose,2000.00,2";
             String expectedFourthLine = "Testing Purpose 2,1031.00,2";
@@ -93,7 +93,7 @@ public class FileOperatorTest {
         try {
             assertEquals(1,app.getAccounts().size());
             assertEquals(31, app.getAccounts().get(0).getTransactions().get(0).getNetAmount());
-            System.setIn(new ByteArrayInputStream("loadtest.csv\nY\n".getBytes()));
+            System.setIn(new ByteArrayInputStream("./data/loadtest.csv\nY\n".getBytes()));
             app.loadAccounts();
             assertNotEquals(1, app.getAccounts().size());
             assertNotEquals(31,app.getAccounts().get(0).getTransactions().get(0).getNetAmount());
